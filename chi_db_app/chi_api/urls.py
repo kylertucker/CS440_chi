011ec8b3354099c41dd5ee20a1ea4f7eeb9d0505
lmwhitehair <lmwhitehair@gmail.com>
2023-04-20 14:03:31 -0400

    add update customerasfdasdf

--- chi_db_app/chi_api/urls.py
+++ chi_db_app/chi_api/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("customer_list", views.customer_list, name='customer_list'),
     path("customer/<int:id>", views.customer, name='customer'),
     path("customer-form", views.customer_form, name='customer_form'),
+    path('customer/<int:id>/update', views.update_customer, name='update_customer'),
+    path('vehicle/<int:id>/add-history', views.add_vehicle_history, name='add_vehicle_history'),
 ]
--- chi_db_app/chi_api/views.py
+++ chi_db_app/chi_api/views.py
@@ -1,5 +1,5 @@
 from django.db import connections
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
@@ -39,6 +39,17 @@ def vehicle(request, id):
     }
     return HttpResponse(template.render(context, request))
 
+def add_vehicle_history(request, id):
+
+  #  template = loader.get_template('chi_api/add_vehicle_history.html')
+
+    histories = []
+
+    context = {
+        "vehicle": vehicle,
+        "histories": histories
+    }
+    return render(request, 'chi_api/add_vehicle_history.html', context)
 
 def customer_list(request):
     cursor = connections['default'].cursor()
@@ -59,11 +70,35 @@ def customer(request, id) :
     transactions = []
     for transaction in customer.vehicletransaction_set.all():
         transactions.append(transaction)
+
+    if request.method == 'POST':
+        customer.name = request.POST.get('name')
+        customer.license_number = request.POST.get('license_number')
+        customer.license_state = request.POST.get('license_state')
+        customer.insurance_provider = request.POST.get('insurance_provider')
+        customer.policy_number = request.POST.get('policy_number')
+        customer.save()
+
     context = {
         "customer": customer,
         "transactions": transactions
     }
     return HttpResponse(template.render(context, request))
+
+def update_customer(request, id):
+    customer = Customer.objects.get(pk=id)
+
+    if request.method == 'POST':
+        customer.name = request.POST['name']
+        customer.license_number = request.POST['license_number']
+        customer.license_state = request.POST['license_state']
+        customer.insurance_provider = request.POST['insurance_provider']
+        customer.policy_number = request.POST['policy_number']
+        customer.save()
+        return redirect('customer', id=id)
+
+    context = {'customer': customer}
+    return render(request, 'chi_api/update_customer.html', context)
 
 
 @csrf_exempt
@@ -83,4 +118,6 @@ def customer_form(request):
 
     template = loader.get_template('chi_api/customer_form.html')
     return HttpResponse(template.render(request=request))
+
+
 
