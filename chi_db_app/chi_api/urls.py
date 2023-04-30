bba9cb78f26715163a76ffcc08245eba506c509d
StephenMabry <sm00010@mix.wvu.edu>
2023-04-30 14:06:01 -0400

    added employee_list.html, employee.html, and added urls and views. Won't print the sql results

--- chi_db_app/chi_api/urls.py
+++ chi_db_app/chi_api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("home", views.home_page, name="home"),
     path("vehicles", views.vehicle_list, name='vehicle_list'),
     path("vehicle/<int:id>", views.vehicle, name='vehicle'),
+    path("vehicle/<int:id>/update", views.update_vehicle, name='update_vehicle'),
     path("customer_list", views.customer_list, name='customer_list'),
     path("customer/<int:id>", views.customer, name='customer'),
     path("customer-form", views.customer_form, name='customer_form'),
